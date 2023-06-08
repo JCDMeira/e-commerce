5e89978ea0d4d3b01f1a9d1eb3cf8afd9143aaa6
Jean Meira <jean.meira10@hotmail.com>
2023-06-08 17:05:20 -0300

    feat: add header structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="App">
+      <div className="header_logo"></div>
+      <div className="header_search"></div>
+      <div className="header_nav">
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Hello Guest</span>
+          <span className="nav_itemLineTwo">Sign in</span>
+        </div>
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Your</span>
+          <span className="nav_itemLineTwo">Shop</span>
+        </div>
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Basket</span>
+          <span className="nav_itemLineTwo">0</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
